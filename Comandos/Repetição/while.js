34391ae25ea744c11041bef3813e34ab3a7a8b80
ecarateu <ecarateu@gmail.com>
2023-04-10 13:48:50 -0300

    add comados exemplos de comandos javascript

---
+++ Comandos/Repetição/while.js
@@ -0,0 +1,7 @@
+//imprime em ordem decrescente todos os números entre 20 e 0, incluindo os limites! No final, escreva a palavra "FIM" .
+var aux = 20;
+while (aux >= 0){
+    document.write(aux + "\n");
+    aux --; 
+}
+document.write ("FIM");
